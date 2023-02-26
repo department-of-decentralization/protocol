e5c2be89aa365b841b7e624102803948a6bad1be
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2023-02-26 23:12:55 +0100

    Init style and content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,174 @@
-import * as React from "react"
+import * as React from "react";
+import refs from "./references";
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+const FootNote = ({ id, author, description, year, url, accessed }) => (
+  <li id={`fn${id}`} className="flex flex-row">
+    <div className="mr-2 leading-5">[{id}]</div>
+    <div className="leading-5">
+      {author && <span>{author}. </span>}
+      {description && <span className="italic">{description}</span>}
+      {year && <span className="italic"> ({year}).</span>}
+      {url && (
+        <>
+          <span className="text-sm"> URL: </span>
+          <a href="{url}" className="font-mono text-xs">
+            {url}.
+          </a>
+        </>
+      )}
+      {accessed && <span> (accessed: {accessed}).</span>}
+      <a href={`#ref${id}`}>↩</a>
+    </div>
+  </li>
+);
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+const Ref = ({ src }) => {
+  return (
+    <sup>
+      <a href={`#fn${src.id}`} id={`ref${src.id}`}>
+        {src.id}
+      </a>{" "}
+    </sup>
+  );
+};
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
+    <main className="my-24">
+      <header className="text-center">
+        <h1 className="font-bold">Protocol Berg</h1>
+        <p className="italic text-center">
+          The decentralized protocol and infrastructure conference
+        </p>
+        <p className="text-center">WORK IN PROGRESS</p>
+        <p className="author">
+          Satoshi Nakamoto, Vitalik Buterin, Juan Benet, Molly Mackinley, Gavin
+          Wood, Robert Habermeier, Ethan Buchanan, Zaki Manian, Jae Kwon, et
+          alia.
+          <div className="mt-8 text-lg">
+            September 15, 2023, Kreuzberg, Berlin
+          </div>
+        </p>
+        <p className="text-center">
+          A Department of Decentralization
+          <Ref src={refs.dod} />
+          Event
+        </p>
+      </header>
+
+      <div className="abstract">
+        <h2 className="font-bold">Abstract</h2>
+        <p>
+          There are various conferences addressing end-user applications and
+          consumer-grade products
+          <Ref src={refs.dappcon} />
+          <Ref src={refs.desci} />
+          <Ref src={refs.blockchainweek} />. However, not many of them manage to
+          create a stage for protocol research, decentralized infrastructure, or
+          core-developer experience. Protocol Berg is a novel summit to address
+          this shortcoming by creating a one-day event with two parallel stages
+          and several opportunities for technical workshops and protocol
+          community gatherings. Attendance is free of charge and the event will
+          not be influenced by sponsors or commercial vendors.
+        </p>
+      </div>
+      <section>
+        <h2 className="font-bold">Conference</h2>
+        <p>
+          Protocol Berg will be a one-day technical conference targeting an
+          audience of protocol engineers, system engineers, network engineers,
+          blockchain operation engineers, decentralized infrastructure
+          administrators, as well as researchers and other curious minds.
+        </p>
+        <p>
+          Topics covered by the event orbit mainly around consensus protocols,
+          distributed virtual machines, peer-to-peer networking, decentralized
+          infrastructure, open-source governance, and protocol research.
+        </p>
+        <p>
+          The venue will be equipped with two stages, one for keynotes,
+          visionary talks, and events, and one for hands-down technical talks.
+          In addition, there will be workshop areas for deep technical study and
+          knowledge-sharing classes.
+        </p>
+        <p>
+          We expect 500-600 attendees and tickets will be free as in{" "}
+          <span className="italic">free lemonade</span>. There will be no
+          sponsors and no swag, or any other distractions whatsoever.
+        </p>
+      </section>
+
+      <section>
+        <h2 className="font-bold">Call for Participation</h2>
+        <p>
+          To submit a technical talk or workshop proposal, use our Pretalx
+          interface: <br />
+          <a href="https://speak.protocol.berlin" className="ml-12">
+            https://speak.protocol.berlin
           </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+        </p>
+        <p>
+          To join our collective as contributor or volunteer, please email us:{" "}
+          <br />
+          <a href="mailto:hello@ethberlin.org" className="ml-12">
+            hello@ethberlin.org{" "}
+          </a>
+        </p>
+      </section>
+
+      <section>
+        <h2 className="font-bold">Venue</h2>
+        <p>
+          The <span className="italic">Heeresbäckerei</span> (magazine in the
+          army bakery) <Ref src={refs.heeresbaeckerei} /> in Berlin-Kreuzberg is
+          an impressive industrial monument located directly at the Spree. The
+          magazine in the west wing was used as a warehouse since 1890, lorries
+          with flour and grain traveled on rails between the magazine and the
+          bakery. The brick building has retained its substance over the years.
+          Cast iron columns carry a five-meter-high ceiling, the parquet is made
+          of old beech, deep arched windows open the room to the light.
+        </p>
+        <p>
+          The result is a magnificent hall with a good deal of charm - perhaps
+          the most beautiful in Berlin-Kreuzberg <Ref src={refs.xberg} />.{" "}
+          <b>Address: Köpenicker Straße 16-17, 10997 Berlin-Kreuzberg</b>.
+        </p>
+      </section>
+
+      <section>
+        <h2 className="font-bold">About the Host</h2>
+        <p>
+          The <span className="italic">Department of Decentralization</span>{" "}
+          <Ref src={refs.dod} /> is a collective of people from various crypto,
+          decentralization, and blockchain communities in and around Berlin. The
+          group first assembled in 2018 to organize events such as ETHBerlin{" "}
+          <Ref src={refs.ethberlin} /> or GoerliCon <Ref src={refs.goerli} />{" "}
+          and has been active since.
+        </p>
+        <p>
+          The aim is to be an agnostic vehicle to drive adoption, educate
+          newcomers, and raise awareness on the challenges and benefits of
+          decentralization and open source software. Currently, the Department
+          is primarily run from Berlin. The collective is composed of around a
+          dozen members and takes decisions using rough consensus.
+        </p>
+      </section>
+      <footer className="footnotes mt-8">
+        <ul>
+          {
+            // make refs object into an array by sorting their id and map over it
+            Object.values(refs)
+              .sort((a, b) => a.id - b.id)
+              .map((ref) => (
+                <FootNote key={ref.id} {...ref} />
+              ))
+          }
+        </ul>
+      </footer>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Home Page</title>;
