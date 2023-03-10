0e33e1733d91287e9e4acdd85df21ca6995f348b
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2023-03-10 09:59:14 +0100

    Update Twitter card

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -23,7 +23,7 @@ export const SEO = ({ title, description, pathname, children }) => {
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
-      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:card" content="summary_image" />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
       <meta name="twitter:description" content={seo.description} />
