52c9d1bcd6e7e77980ec215d7d60c274f00cc559
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2023-03-12 19:40:59 +0100

    Update meta tags

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -23,11 +23,12 @@ export const SEO = ({ title, description, pathname, children }) => {
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
+      <meta name="og:title" content={seo.title} />
+      <meta name="og:url" content={seo.url} />
+      <meta name="og:description" content={seo.description} />
+      <meta name="og:image" content={seo.image} />
+      <meta name="twitter:site" content={seo.twitterUsername} />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={seo.title} />
-      <meta name="twitter:url" content={seo.url} />
-      <meta name="twitter:description" content={seo.description} />
-      <meta name="twitter:image" content={seo.image} />
       <meta name="twitter:creator" content={seo.twitterUsername} />
       {children}
     </>
