f4cfbf21340bd044e26b134a95b4acd9574dc91d
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2023-03-13 20:39:25 +0100

    Blur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ["Courier Prime", ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 };
