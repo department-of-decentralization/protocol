09e2ecf293a2fb873b54734dd21d8058d6af5062
Kaan Uzdogan <kaanuzdogan@hotmail.com>
2023-03-14 15:08:17 +0100

    Revert "Blur"
    This reverts commit f4cfbf21340bd044e26b134a95b4acd9574dc91d.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   content: [
     "./src/pages/**/*.{js,jsx,ts,tsx}",
     "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        mono: ["Courier Prime", ...defaultTheme.fontFamily.mono],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
