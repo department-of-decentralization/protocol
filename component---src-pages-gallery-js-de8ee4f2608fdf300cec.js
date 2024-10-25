e9efd815daf05d7b758cd116cbdfc73493c627af
kuzdogan <kuzdogan@users.noreply.github.com>
2024-10-25 12:40:31 +0000

    deploy: f391887f1fd197f619ed3e4349d16473dae8547e

---
+++ component---src-pages-gallery-js-de8ee4f2608fdf300cec.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkprotocol_berlin=self.webpackChunkprotocol_berlin||[]).push([[822],{62:function(e,l,t){t.r(l);var o=t(7294),r=t(1883);l.default=()=>((0,o.useEffect)((()=>{(0,r.navigate)("http://2023.protocol.berlin/gallery/")}),[]),null)}}]);
+//# sourceMappingURL=component---src-pages-gallery-js-de8ee4f2608fdf300cec.js.map
