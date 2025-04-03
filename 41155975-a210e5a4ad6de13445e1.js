789a199c31f72f1a51e302e11a1dfc29b0cfeaf9
kuzdogan <kuzdogan@users.noreply.github.com>
2025-04-03 11:54:00 +0000

    deploy: 8cea2c6f4de2a6ae816c7e530c988680b82e47eb

---
+++ 41155975-a210e5a4ad6de13445e1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkprotocol_berlin=self.webpackChunkprotocol_berlin||[]).push([[396],{6204:function(t,n,r){r.d(n,{LCd:function(){return e}});var c=r(3621);function e(t){return(0,c.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=41155975-a210e5a4ad6de13445e1.js.map
