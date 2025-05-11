8bc056e78f839339dd03c5430edffbb439d4c00f
q9f <q9f@users.noreply.github.com>
2025-05-11 16:23:37 +0000

    deploy: 4e6316c6b9dfdec2ae4c7cc3b5cac5196ace07aa

---
+++ fea29d9f-377b8fbf97ee00192aed.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkprotocol_berlin=self.webpackChunkprotocol_berlin||[]).push([[838],{80:function(t,l,n){n.d(l,{Hrt:function(){return a}});var r=n(3621);function a(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"},child:[]},{tag:"path",attr:{d:"M22 3.41 16.71 8.7 20 12h-8V4l3.29 3.29L20.59 2 22 3.41zM3.41 22l5.29-5.29L12 20v-8H4l3.29 3.29L2 20.59 3.41 22z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=fea29d9f-377b8fbf97ee00192aed.js.map
